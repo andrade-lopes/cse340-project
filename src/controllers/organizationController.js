d5d19ca5882c11d1fc11ba9641d8063d1fc02a8a
andrade-lopes <jlopes@byupathway.edu>
2026-06-03 20:38:13 -0100

    update

--- src/controllers/organizationController.js
+++ src/controllers/organizationController.js
@@ -1,4 +1,22 @@
+import organizationsModel from "../models/organizations.js";
+
 export const showNewOrganizationForm = async (req, res) => {
     const title = "Add New Organization";
     res.render("new-organization", { title });
+};
+
+export const processNewOrganizationForm = async (req, res) => {
+    const { name, description, contactEmail } = req.body;
+
+    const logoFilename = "placeholder-logo.png";
+
+    const organizationId =
+        await organizationsModel.createOrganization(
+            name,
+            description,
+            contactEmail,
+            logoFilename
+        );
+
+    res.redirect(`/organization/${organizationId}`);
 };
--- src/routes/organizationRoutes.js
+++ src/routes/organizationRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { showNewOrganizationForm } from "../controllers/organizationController.js";
+import {
+    showNewOrganizationForm,
+    processNewOrganizationForm
+} from "../controllers/organizationController.js";
 
 const router = express.Router();
 
+// Display form
 router.get("/new-organization", showNewOrganizationForm);
+
+// Handle form submission
+router.post("/new-organization", processNewOrganizationForm);
 
 export default router;
