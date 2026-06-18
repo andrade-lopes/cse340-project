81960cb62924612837b4ae6a5cc3320d33d0b114
andrade-lopes <jlopes@byupathway.edu>
2026-06-18 17:34:05 -0100

    DashboardRoutes.js created

---
+++ src/routes/dashboardRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dashboardController from '../controllers/dashboardController.js';
+import { checkLogin } from '../middleware.js';
+
+const router = express.Router();
+
+router.get(
+    '/dashboard',
+    checkLogin,
+    dashboardController.showDashboard
+);
+
+export default router;
