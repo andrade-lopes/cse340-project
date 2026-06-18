d64cf78a73f0b464a882ad4b5c6c6fca643e799c
andrade-lopes <jlopes@byupathway.edu>
2026-06-18 18:07:34 -0100

    ProjectModel.js updated

--- src/models/projectModel.js
+++ src/models/projectModel.js
@@ -1,16 +1,32 @@
+import db from './db.js';
+
 async function getProjectById(projectId) {
     const sql = `
-        SELECT *
-        FROM projects
-        WHERE project_id = $1
+        SELECT p.*, o.name AS organization_name
+        FROM projects p
+        LEFT JOIN organization o
+            ON p.organization_id = o.organization_id
+        WHERE p.project_id = $1
     `;
 
-    const result =
-        await pool.query(sql, [projectId]);
-
+    const result = await db.query(sql, [projectId]);
     return result.rows[0];
 }
 
+async function getAllProjects() {
+    const sql = `
+        SELECT p.*, o.name AS organization_name
+        FROM projects p
+        LEFT JOIN organization o
+            ON p.organization_id = o.organization_id
+        ORDER BY p.project_date ASC
+    `;
+
+    const result = await db.query(sql);
+    return result.rows;
+}
+
 export default {
-    getProjectById
+    getProjectById,
+    getAllProjects
 };
