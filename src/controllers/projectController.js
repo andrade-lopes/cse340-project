9e867deb8421acb422b972b228438bb51c9c648f
andrade-lopes <jlopes@byupathway.edu>
2026-06-18 18:20:40 -0100

    ProjectController.js updated

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,38 +1,49 @@
-import categoryModel from "../models/categoryModel.js";
+import projectModel from '../models/projectModel.js';
+import volunteerModel from '../models/volunteer-model.js';
 
 async function getProjects(req, res) {
     try {
+        const projects = await projectModel.getAllProjects();
 
-        const categories =
-            await categoryModel.getCategoriesByProjectId(null);
-
-        res.render("projects", {
-            title: "Projects",
-            categories
+        res.render('projects', {
+            title: 'Service Projects',
+            projects
         });
 
     } catch (error) {
         console.error(error);
-        res.status(500).send("Server error");
+        res.status(500).send('Server error');
     }
 }
 
 async function getProjectDetails(req, res) {
     try {
-
         const projectId = req.params.projectId;
 
-        const project =
-            await projectModel.getProjectById(projectId);
+        const project = await projectModel.getProjectById(projectId);
 
-        res.render("project-details", {
+        if (!project) {
+            return res.status(404).send('Project not found');
+        }
+
+        let isVolunteer = false;
+
+        if (req.session && req.session.user) {
+            isVolunteer = await volunteerModel.isVolunteer(
+                req.session.user.user_id,
+                projectId
+            );
+        }
+
+        res.render('project-details', {
             title: project.project_name,
-            project
+            project,
+            isVolunteer
         });
 
     } catch (error) {
         console.error(error);
-        res.status(500).send("Server error");
+        res.status(500).send('Server error');
     }
 }
 
