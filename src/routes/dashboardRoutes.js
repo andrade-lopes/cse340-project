9945ceef4109dcea0f31c8d23f176f48de76d2af
andrade-lopes <jlopes@byupathway.edu>
2026-06-23 22:00:52 -0100

    update

--- src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import dashboardController from '../controllers/dashboardController.js';
-import { requireLogin } from '../middleware.js';
-
-const router = express.Router();
-
-router.get('/dashboard', requireLogin, dashboardController.showDashboard);
-
-export default router;
--- src/routes/projectRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import projectController from '../controllers/projectController.js';
-
-const router = express.Router();
-
-router.get('/projects', projectController.getProjects);
-router.get('/projects/:projectId', projectController.getProjectDetails);
-
-export default router;
